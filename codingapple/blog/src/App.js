a3ef107debdbfc133f6d1296dcf7af4cd45f0886
system-out-print-jiyeon <67897652+system-out-print-jiyeon@users.noreply.github.com>
2022-03-18 17:00:12 +0900

    onClick

--- codingapple/blog/src/App.js
+++ codingapple/blog/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */ 
+
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -10,14 +12,16 @@ function App() {
   }
 
   let [글제목,글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '가나다라마바사']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div style={{color:'pink', fontSize:'30px'}}>개발 blog</div>
       </div>
+      <button onClick={ () => {} }>button</button>
       <div className='list'>
-        <h4> { 글제목[0] } </h4>
+        <h4> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) }}> 👍 </span>{따봉}</h4>
         <p>2월 17일 발행</p>
         <hr/>
       </div> 
