37ed957c97e655aa546dd462868dba944bc0e173
system-out-print-jiyeon <67897652+system-out-print-jiyeon@users.noreply.github.com>
2022-03-21 16:59:49 +0900

    shop

---
+++ codingapple/shop/src/App.js
@@ -0,0 +1,70 @@
+import logo from './logo.svg';
+import {Button, Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import './App.css';
+import { useState } from 'react';
+import Data from './data.js';
+
+function App() {
+
+  let [shoes, shoes변경] = useState(Data);
+
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className='background'>
+        <h1>Hello, world!</h1>
+        <p>
+          This is simple 어쩌구 ~~~~
+        </p>
+        <p>
+          <Button variant="primary">Primary</Button>{' '}
+        </p>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <h4>{ shoes[0].title }</h4>
+            <p>{ shoes[0].content }</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
