26eaf0b3affb32242e65e0e9e268a248480fd6d7
system-out-print-jiyeon <67897652+system-out-print-jiyeon@users.noreply.github.com>
2022-04-01 16:58:05 +0900

    header router

--- gabozago/src/App.tsx
+++ gabozago/src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h3>가보자 가보자</h3>
+    <Router>
+      <Header/>
+      <div className="App">
+        <h3>가보자 가보자</h3>
+      </div>
+    </Router>
+  );
+}
+
+
+function Header() {
+
+  return (
+    <div className="header">
+        <h3>GABOZAGO</h3>
     </div>
   );
 }
