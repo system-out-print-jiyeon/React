04b8b5676c7557a0229faf0ae6292e9ef77f3ede
Jiyeon <jy971114@naver.com>
2022-04-08 00:52:29 +0900

    time input

--- gabozago/src/components/Timer.tsx
+++ gabozago/src/components/Timer.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { FC, useCallback } from 'react';
 import Moment from 'react-moment';
 import moment from 'moment';
 import { Duration } from 'moment';
 
-function Timer(){
 
-    /* const now = moment().format('HH:mm:ss');
-    console.log("now : " + now);
-    const paramTime = '18:05' + ':00';
-    console.log("paramTime : " + paramTime); */
-    const durationToHMS = (duration: Duration) => {
-        const hour = duration.get('hours');
-        const min = duration.get('minutes');
-        const sec = duration.get('seconds');
-      
-        return { hour, min, sec };
-      };
+export interface IProps {
+    inputTime: string
+}
+
+const Timer: FC<IProps> = ({
+    inputTime
+}:IProps) => {
+    
+    
+
     return (
         <div className="timer">
-            durationToHMS();
+            퇴근시간 {inputTime} 까지...
         </div>
     );
 }
